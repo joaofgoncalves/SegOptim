16d19217ee34e3e777f51f380fe0a5867f736f22
JoaoGoncalves <joaofgo@gmail.com>
2020-06-06 18:41:45 +0100

    Adds new test for data simulation

--- tests/testthat/test-SimulateTestData.R
+++ tests/testthat/test-SimulateTestData.R
@@ -6,7 +6,6 @@ library(SegOptim)
 library(raster)
 
 
-
 test_that("Simulate raster train data",{
 
   expect_is(simRasterTrain(),"RasterLayer")  
@@ -21,6 +20,13 @@ test_that("Simulate raster segments data",{
 })
 
 
+test_that("Simulate raster segments data",{
+  
+  expect_is(simRasterSegments2(),"RasterLayer")  
+  
+})
+
+
 test_that("Simulate raster features data",{
   
   expect_is(simRasterFeatures(),"RasterStack")  
@@ -30,7 +36,8 @@ test_that("Simulate raster features data",{
 
 test_that("Simulate raster features data",{
   
-  expect_is(simRasterFeatures(distr = "runif", args = list(n=10000, 0, 1)),"RasterStack")  
+  expect_is(simRasterFeatures(distr = "runif", 
+                              args = list(n=10000, 0, 1)),"RasterStack")  
   
 })
 

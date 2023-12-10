22759be644adfc35054db4724aeef130f8eff6b1
João Gonçalves <joaofgo@gmail.com>
2023-12-10 11:59:30 +0000

    Delete files

--- GRASS_Segm_BatchCmdRun_6c1c79c76163.bat
+++
@@ -1,3 +0,0 @@
-set GRASS_BATCH_JOB=C:/MyFiles/R-dev/SegOptim/GRASS_Segm_BatchCmd_6c1c6bca2e2c.bat
-
-"C:/Program Files/QGIS 3.32.3/bin/grass83" C:/Users/JG/Documents/grassdata/TestSites_SOptim/PERMANENT -text
